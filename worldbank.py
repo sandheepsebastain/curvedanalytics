a51469118a7d00d20f5790a8d77a33895c938f74
sandheepsebastian <sandheep.sebastian@woodmac.com>
2022-03-30 21:17:03 -0500

    Adding headers to chatbot response so it can be called as an API part2

--- worldbank.py
+++ worldbank.py
@@ -1,4 +1,4 @@
 from worldbankapp import app
 #Comment below line before merging to github which would deploy it to heroku
 #Uncomment below line to run on DEV
-app.run('0.0.0.0',debug=True,port=3000,threaded=True)
+#app.run('0.0.0.0',debug=True,port=3000,threaded=True)
