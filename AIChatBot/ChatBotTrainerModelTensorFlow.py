00b457406107a09e87c4fe2e4f7894b8c3a1e3bd
sandheepsebastian <sandheep.sebastian@woodmac.com>
2022-04-02 09:26:46 -0500

    Improving the dialogue by updating content.json and retraining the model

--- AIChatBot/ChatBotTrainerModelTensorFlow.py
+++ AIChatBot/ChatBotTrainerModelTensorFlow.py
@@ -18,7 +18,8 @@ import pickle
 
 
 
-#importing the dataset
+#importing the chat dataset
+#Storing the questions/inputs and the corresponding tags into the data dataframe
 with open('content.json') as content:
   data1 = json.load(content)
 #getting all the data to lists
@@ -35,20 +36,31 @@ data = pd.DataFrame({"inputs":inputs,
                      "tags":tags})
 print(data)
 
-#removing punctuations
+###########Inputs transformation#######################
+#Cleaning up the inputs to make it ready for training
 import string
+#Converting all to lower case and removing punctuations if they exist
 data['inputs'] = data['inputs'].apply(lambda wrd:[ltrs.lower() for ltrs in wrd if ltrs not in string.punctuation])
+
+#putting in back to gether
 data['inputs'] = data['inputs'].apply(lambda wrd: ''.join(wrd))
+
 #tokenize the data
 from tensorflow.keras.preprocessing.text import Tokenizer
+#Tokenizer num words tell the text_to_sequence function how many of the top num_words you want to convert
+#If num_words is 1 the text_to_sequence will only return a vectorized sequence with the top 1 word
+#fit_on_text will provide a word index. Giving an id for each word in your corpus. In this case the chat json
+#texts_to_sequence will use this word index to convert all our sentences to a number vector
 tokenizer = Tokenizer(num_words=2000)
 tokenizer.fit_on_texts(data['inputs'])
 train = tokenizer.texts_to_sequences(data['inputs'])
 
 #apply padding
+#Padding makes sure all the sentence vectors are of the same size
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 x_train = pad_sequences(train)
 
+#######################Tags Transformation#############
 #encoding the outputs
 from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder()
