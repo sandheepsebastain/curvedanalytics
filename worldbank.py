70063cb60777dd56fce45e50d2839d3244e0d6f2
sandheepsebastian <sandheep.sebastian@woodmac.com>
2022-09-23 16:34:28 -0400

    figuring out start point part2

--- worldbank.py
+++ worldbank.py
@@ -1,1 +1,4 @@
-from worldbankapp import startpoint
+from worldbankapp import app
+#Comment below line before merging to github which would deploy it to heroku
+#Uncomment below line to run on DEV
+#app.run('0.0.0.0',debug=True,port=5000,threaded=True)
